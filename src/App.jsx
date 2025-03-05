8ddfa57f48850ef5802c7f0285e8b38f37932847
BrockSpacek <brockspacek1@gmail.com>
2025-03-05 15:26:36 -0800

    Large Card Data Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-
+import { Button } from 'flowbite-react'
 import './App.css'
+import LargeCard from './components/LargeCard'
+import SmallCard from './components/SmallCard'
 
 function App() {
  
 
   return (
-      <h1>Hello World</h1>
+  <div>
+    <h1>Social Media Dashboard</h1>
+    <p>Total Followers: 23,004</p>
+    <Button />
+      <LargeCard />
+      <h1 className='text-5xl '>Overview - Today</h1>
+      <SmallCard />
+  </div>
   )
 }
 
--- src/components/LargeCard.jsx
+++ src/components/LargeCard.jsx
@@ -1,9 +1,105 @@
-import React from 'react'
+import { Card } from "flowbite-react";
+import FaceBookIcon from "../assets/icon-facebook.svg"
+import UpIcon from "../assets/icon-up.svg"
+import DownIcon from "../assets/icon-down.svg"
+import InstagramIcon from "../assets/icon-instagram.svg"
+import YoutubeIcon from "../assets/icon-youtube.svg"
+import TwitterIcon from "../assets/icon-twitter.svg"
 
-const LargeCard = () => {
+export default function LargeCard() {
+
+
+
+
+
+
   return (
-    <div>LargeCard</div>
-  )
-}
+   <div className="flex justify-evenly">
+    <Card className="lg:w-[275px]">
 
-export default LargeCard
+        <div className="flex justify-center">
+            <img className="h-6 w-6 mr-1" src={FaceBookIcon}></img>
+            <h5 className="ml-1">
+            @nathanf
+            </h5>
+        </div>
+        <div className="text-center">
+            <p>1987</p>
+            <p>Followers</p>
+        </div>
+        <div className="flex justify-center">
+            <div className="flex justify-content-center mt-2">
+                <img className="h-2 w-2" src={UpIcon}></img>
+            </div>
+            <p>12 Today</p>
+        </div>
+    
+    </Card>
+
+    <Card className="lg:w-[275px]">
+
+        <div className="flex justify-center">
+            <img className="h-6 w-6 mr-1" src={TwitterIcon}></img>
+            <h5 className="ml-1">
+            @nathanf
+            </h5>
+        </div>
+        <div className="text-center">
+            <p>1044</p>
+            <p>Followers</p>
+        </div>
+        <div className="flex justify-center">
+            <div className="flex justify-content-center mt-2">
+                <img className="h-2 w-2" src={UpIcon}></img>
+            </div>
+            <p>99 Today</p>
+        </div>
+    
+    </Card>
+
+    <Card className="lg:w-[275px]">
+
+        <div className="flex justify-center">
+            <img className="h-6 w-6 mr-1" src={InstagramIcon}></img>
+            <h5 className="ml-1">
+            @realnathanf
+            </h5>
+        </div>
+        <div className="text-center">
+            <p>11k</p>
+            <p>Followers</p>
+        </div>
+        <div className="flex justify-center">
+            <div className="flex justify-content-center mt-2">
+                <img className="h-2 w-2" src={UpIcon}></img>
+            </div>
+            <p>1099 Today</p>
+        </div>
+    
+    </Card>
+
+    <Card className="lg:w-[275px]">
+
+        <div className="flex justify-center">
+            <img className="h-6 w-6 mr-1" src={YoutubeIcon}></img>
+            <h5 className="ml-1">
+             Nathan F.
+            </h5>
+        </div>
+        <div className="text-center">
+            <p>8239</p>
+            <p>Followers</p>
+        </div>
+        <div className="flex justify-center">
+            <div className="flex justify-content-center mt-2">
+                <img className="h-2 w-2" src={DownIcon}></img>
+            </div>
+            <p>144 Today</p>
+        </div>
+    
+    </Card>
+
+
+    </div> 
+  );
+}
